65542a3243b28b8c6cfb6429de25029d39b52d73
john-minney-iii <jminneyiii@gmail.com>
2022-03-08 22:32:03 -0700

    Added user auth

--- server/user/urls.py
+++ server/user/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from .views import RegisterUser
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('register/', RegisterUser.as_view(), name='register-user'),
+    path('auth/', views.obtain_auth_token, name='api-token-auth'),
 ]

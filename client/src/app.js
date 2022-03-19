2b5ceb892058f259d7986f53a107e4e39f01b2a7
john-minney-iii <jminneyiii@gmail.com>
2022-03-18 18:01:04 -0600

    Base stylesheets

--- client/src/app.js
+++ client/src/app.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Navbar from './components/navbar';
+import './assets/css/app.css';
 
 export default class App extends Component {
     render() {
         return(
             <div className='app m-5'>
                 <Navbar authenticated={true} />
-                <h1>GCODEdeck</h1>
             </div>
         );
     }

b8118882329ddf4a86b33359b833f31be08aed11
john-minney-iii <jminneyiii@gmail.com>
2022-03-18 21:38:17 -0600

    Basic user reg, auth, and del

--- client/src/app.js
+++ client/src/app.js
@@ -1,12 +1,81 @@
 import React, { Component } from 'react';
 import Navbar from './components/navbar';
+import axios from 'axios';
 import './assets/css/app.css';
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            token: '',
+            loggedIn: false
+        };
+    }
+
+    registerTestUser = () => {
+        axios.post('/api/v1/user/register/', {
+            'username': 'test',
+            'first_name': 'first-name-test',
+            'last_name': 'last-name-test',
+            'email': 'test@domain.com',
+            'password': 'testpass'   
+         }).then(res => {
+            console.log('Register');
+            if (res.status === 201)
+                this.setState({ username: 'test' });
+        });
+    };
+
+    loginOnClick = () => {
+        console.log('Login Clicked');
+        axios.post('/api/v1/user/auth/', {
+            "username": "test",
+            "password": "testpass"
+        }).then(res => {
+            if (res.status === 200)
+                this.setState({ token: res.data.token, loggedIn: true });
+        });
+    };
+
+    deleteOnClick = () => {
+        console.log('Delete Clicked');
+        axios.post('/api/v1/user/delete/', {
+            "username": "test",
+            "password": "testpass",
+            "token": this.state.token
+        }, {
+            headers: {
+                "Authorization": "TOKEN " + this.state.token
+            }
+        }).then(res => {
+            if (res.status === 200) {
+                this.setState(
+                    {
+                        username: '',
+                        token: '',
+                        loggedIn: false
+                    }
+                );
+            }
+        });
+    };
+
+    data = () => {
+        console.log('Username: ' + this.state.username);
+        console.log('Token: ' + this.state.token);
+    };
+
     render() {
         return(
             <div className='app m-5'>
-                <Navbar authenticated={false} />
+                <Navbar authenticated={this.state.loggedIn} />
+                <div className='m-5'>
+                    <a className='btn btn-primary mx-2' onClick={this.registerTestUser}>Register User</a>
+                    <a className='btn btn-primary mx-2' onClick={this.loginOnClick}>Test Login</a>
+                    <a className='btn btn-primary mx-2' onClick={this.deleteOnClick}>Test Delete</a>
+                    <a className='btn btn-primary mx-2' onClick={this.data}>Data</a>
+                </div>
             </div>
         );
     }
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -3,34 +3,34 @@ import React, { Component } from 'react';
 export default function Navbar(props) {
     if (props.authenticated) {
         return(
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <a class="navbar-brand mx-2" href="/">GCODEdeck</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <a className="navbar-brand mx-2" href="/">GCODEdeck</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse mx-2" id="navbarNavAltMarkup">
-                    <div class="navbar-nav ms-auto">
-                        <a href="/" class="nav-item nav-link active" id="homeNavLink">Home</a>
-                        <a href="" class="nav-item nav-link" id="myProgramsNavLink">My Programs</a>
-                        <a href="" class="nav-item nav-link" id="myToolsNavLink">My Tools</a>
-                        <a href="" class="nav-item nav-link" id="communityNavLink">Community</a>
-                        <a href='' class="nav-item nav-link" id="accountNavLink">Account</a>
+                <div className="collapse navbar-collapse mx-2" id="navbarNavAltMarkup">
+                    <div className="navbar-nav ms-auto">
+                        <a href="/" className="nav-item nav-link active" id="homeNavLink">Home</a>
+                        <a href="" className="nav-item nav-link" id="myProgramsNavLink">My Programs</a>
+                        <a href="" className="nav-item nav-link" id="myToolsNavLink">My Tools</a>
+                        <a href="" className="nav-item nav-link" id="communityNavLink">Community</a>
+                        <a href='' className="nav-item nav-link" id="accountNavLink">Account</a>
                     </div>
                 </div>
             </nav>
         );
     } else {
         return(
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <a class="navbar-brand mx-2" href="/">GCODEdeck</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <a className="navbar-brand mx-2" href="/">GCODEdeck</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse mx-2" id="navbarNavAltMarkup">
-                    <div class="navbar-nav ms-auto">
-                        <a href="/" class="nav-item nav-link active" id="homeNavLink">Home</a>
-                        <a href="/community/aboutus/" class="nav-item nav-link" id="aboutusNavLink">About Us</a>
-                        <a href="/community/" class="nav-item nav-link" id="communityNavLink">Community</a>
+                <div className="collapse navbar-collapse mx-2" id="navbarNavAltMarkup">
+                    <div className="navbar-nav ms-auto">
+                        <a href="/" className="nav-item nav-link active" id="homeNavLink">Home</a>
+                        <a href="/community/aboutus/" className="nav-item nav-link" id="aboutusNavLink">About Us</a>
+                        <a href="/community/" className="nav-item nav-link" id="communityNavLink">Community</a>
                     </div>
                 </div>
             </nav>

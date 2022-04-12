dc6d7137400beae727aeb198cceeec1ab6cfa7b8
john-minney-iii <jminneyiii@gmail.com>
2022-04-11 21:14:41 -0600

    Started server upgrade

--- server/gcode/urls.py
+++ server/gcode/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+from .views import ToolChange, SpindleCommand, RapidMovement, \
+    LinearMovement, Drilling, FacingTemplate, RectangleTemplate
 
 urlpatterns = [
-    
+    path('', ToolChange.as_view(), name='tool-change'),
+    path('', SpindleCommand.as_view(), name='spindle-command'),
+    path('', RapidMovement.as_view(), name='rapid-movement'),
+    path('', LinearMovement.as_view(), name='linear-movement'),
+    path('', Drilling.as_view(), name='drilling'),
+    path('', FacingTemplate.as_view(), name='facing-template'),
+    path('', RectangleTemplate.as_view(), name='rectangle-template'),
 ]

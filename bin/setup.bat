6ea74ab49bfe0d4b6b2e4792fbb3ba16f26064b1
john-minney-iii <jminneyiii@gmail.com>
2022-04-12 15:50:09 -0600

    Wait for commands

--- bin/setup.bat
+++ bin/setup.bat
@@ -1,13 +1,13 @@
 @echo off
 
 echo Creating Virtual Environment and Install Libraries...
-start cmd.exe /C "cd .. && py -3.10 -m venv .venv && .venv\Scripts\activate.bat && cd server && python -m pip install --upgrade pip && pip install --upgrade setuptools && git checkout master && python -m pip install -r requirements.txt && exit"
+start /b /wait cmd.exe /C "cd .. && py -3.10 -m venv .venv && .venv\Scripts\activate.bat && cd server && python -m pip install --upgrade pip && pip install --upgrade setuptools && git checkout master && python -m pip install -r requirements.txt && exit"
 
 echo Creating/Updating Datbase for Server...
-start cmd.exe /C "cd .. && .venv\Scripts\activate.bat && cd server && python manage.py makemigrations && python manage.py migrate && exit"
+start /b /wait cmd.exe /C "cd .. && .venv\Scripts\activate.bat && cd server && python manage.py makemigrations && python manage.py migrate && exit"
 
 echo Setting Up React App...
-start cmd.exe /C "cd .. && cd client && npm install && exit"
+start /b /wait cmd.exe /C "cd .. && cd client && npm install && exit"
 
 echo Project Setup Finished
 echo Press Any Key to Exit

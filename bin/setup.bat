c09d074668fa16dff2752f38d45bf721346d7932
john-minney-iii <jminneyiii@gmail.com>
2022-04-12 15:45:02 -0600

    Fixed setup

--- bin/setup.bat
+++ bin/setup.bat
@@ -1,30 +1,15 @@
+@echo off
 
-:start
-cls
+echo Creating Virtual Environment and Install Libraries...
+start cmd.exe /C "cd .. && py -3.10 -m venv .venv && .venv\Scripts\activate.bat && cd server && python -m pip install --upgrade pip && pip install --upgrade setuptools && git checkout master && python -m pip install -r requirements.txt && exit"
 
-cd ..
-echo Creating Virtual Environment
-py -3.10 -m venv .venv
-.venv\Scripts\activate.bat
+echo Creating/Updating Datbase for Server...
+start cmd.exe /C "cd .. && .venv\Scripts\activate.bat && cd server && python manage.py makemigrations && python manage.py migrate && exit"
 
-echo Setting Up Django Server
-cd server
-python -m pip install --upgrade pip
-pip install --upgrade setuptools
-git checkout master
-echo Installing Requirements
-python -m pip install -r requirements.txt
-
-echo Creating/Updating Database for Server
-python manage.py makemigrations
-python manage.py migrate
-
-echo Setting Up React App
-cd ..
-cd client
-npm install
+echo Setting Up React App...
+start cmd.exe /C "cd .. && cd client && npm install && exit"
 
 echo Project Setup Finished
 echo Press Any Key to Exit
 pause
-exit.
+exit

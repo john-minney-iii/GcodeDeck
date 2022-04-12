a04b5eb439508abbdb177f373864854189147983
john-minney-iii <jminneyiii@gmail.com>
2022-04-11 21:36:01 -0600

    Setup urls for gcode

--- server/gcode/urls.py
+++ server/gcode/urls.py
@@ -3,11 +3,11 @@ from .views import ToolChange, SpindleCommand, RapidMovement, \
     LinearMovement, Drilling, FacingTemplate, RectangleTemplate
 
 urlpatterns = [
-    path('', ToolChange.as_view(), name='tool-change'),
-    path('', SpindleCommand.as_view(), name='spindle-command'),
-    path('', RapidMovement.as_view(), name='rapid-movement'),
-    path('', LinearMovement.as_view(), name='linear-movement'),
-    path('', Drilling.as_view(), name='drilling'),
-    path('', FacingTemplate.as_view(), name='facing-template'),
-    path('', RectangleTemplate.as_view(), name='rectangle-template'),
+    path('toolChange/', ToolChange.as_view(), name='tool-change'),
+    path('spindleCommand/', SpindleCommand.as_view(), name='spindle-command'),
+    path('rapidMovement/', RapidMovement.as_view(), name='rapid-movement'),
+    path('linearMovement/', LinearMovement.as_view(), name='linear-movement'),
+    path('drilling/', Drilling.as_view(), name='drilling'),
+    path('facingTemplate/', FacingTemplate.as_view(), name='facing-template'),
+    path('rectangleTemplate/', RectangleTemplate.as_view(), name='rectangle-template'),
 ]

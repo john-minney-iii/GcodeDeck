9d0703543a33a1a955bd6749fa6b112860d060fe
john-minney-iii <jminneyiii@gmail.com>
2022-04-19 19:06:03 -0600

    Moved around some code

---
+++ client/src/views/old/InputForm.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "../assets/css/landing-page.css"
+
+const InputForm = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
+  return (
+    <div className="InputForm">
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
+    </div>
+  );
+};
+
+export default InputForm;

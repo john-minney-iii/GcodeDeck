bf0105c81b38179b435fa6842c0cffe187cf5cb0
Katie <galaxyventurer2000@gmail.com>
2022-04-21 17:11:18 -0600

    linear movement works

--- client/src/components/gen-home-modals/linear-movement-modal.js
+++ client/src/components/gen-home-modals/linear-movement-modal.js
@@ -3,24 +3,21 @@ import { Modal } from "react-bootstrap";
 
 export default function LinearForm(props) {
     const [linearChoice, setLinearChoice] = useState('X');
-    const [linearFeedRate, setLinearFeedRate] = useState(0);
-    const [linearPos, setLinearPos] = useState(0);
-    const [linearPos2, setLinearPos2] = useState(0);
+    const [linearFeedRate, setLinearFeedRate] = useState();
+    const [linearPos, setLinearPos] = useState();
 
     // Validations 
     let linearChoiceValid = false;
     let linearFeedRateValid = false;
     let linearPosValid = false;
-    let linearPos2Valid = false;
 
     const resetFormValues = () => {
         setLinearChoice('');
         setLinearFeedRate('');
         setLinearPos('');
-        setLinearPos2('');
     };
 
-    const LinearChoiceValidation = () => {
+    const linearChoiceValidation = () => {
         if (linearChoice != 'X') {
             linearChoiceValid = false;
             return <small className="text-danger">Please Enter Your Choice</small>;
@@ -47,14 +44,6 @@ export default function LinearForm(props) {
         linearPosValid = true;
     };
 
-    const linearPos2Validation = () => {
-        if (linearPos2 === '') {
-            linearPos2Valid = false;
-            return <small className="text-danger">Please Enter A Valid End Point</small>;
-        }
-        linearPos2Valid = true;
-    };
-
     const LinearForm = () => <form>
         <div className="form-group">
             <label htmlFor="axisOfMovement">Axis of Movement: </label>
@@ -64,7 +53,7 @@ export default function LinearForm(props) {
                 <option value="Z">Z</option>
                 <option value="XY">XY</option>
             </select>
-            {linearFormHelper()}
+            {linearChoiceValidation()}
         </div>
         <div className="form-group">
             <label htmlFor="username-input">Linear Feed Rate</label>
@@ -78,7 +67,7 @@ export default function LinearForm(props) {
             {linearFeedRateValidation()}
         </div>
         <div className="form-group">
-            <label htmlFor="username-input">Username*</label>
+            <label htmlFor="username-input">Position:</label>
             <input 
                 type="text"
                 className="form-control"
@@ -87,63 +76,17 @@ export default function LinearForm(props) {
                 onChange={(e) => setLinearPos(e.target.value)}
             />
             {linearPosValidation()}
-            <small className='text-muted'>
-                150 characters or fewer. Letters, digits and @/_/+/- only
-            </small>
-        </div>
-        <div className="form-group">
-            <label htmlFor="username-input">Email*</label>
-            <input 
-                type="text"
-                className="form-control"
-                name="username-input"
-                value={linearPos2}
-                onChange={(e) => setLinearPos2(e.target.value)}
-            />
-            {linearPos2Validation()}
         </div>
     </form>;
 
-const linearFormHelper = () => {
-    let posInput;
-    if (linearChoice === 'X')
-        posInput = <div>
-            <label htmlfor='x-pos-input'>X:</label>
-            <input className='form-control' name='x-pos-input' onChange={(e) => setLinearPos(e.target.value)} />
-        </div>;
-    else if (linearChoice === 'Y')
-        posInput = <div>
-            <label htmlfor='y-pos-input'>Y:</label>
-            <input className='form-control' name='y-pos-input' onChange={(e) => setLinearPos(e.target.value)} />
-        </div>;
-    else if (linearChoice === 'Z')
-        posInput = <div>
-            <label htmlfor='z-pos-input'>Z:</label>
-            <input className='form-control' name='z-pos-input' onChange={(e) => setLinearPos(e.target.value)} />
-        </div>;
-    else if (linearChoice === 'XY')
-        posInput = <div>
-            <label htmlfor='x-pos-input'>X:</label>
-            <input className='form-control' name='x-pos-input' onChange={(e) => setLinearPos(e.target.value)} />
-            <label htmlfor='y-pos-input'>Y:</label>
-            <input className='form-control' name='y-pos-input' onChange={(e) => setLinearPos2(e.target.value)} />
-        </div>;
-    return <div>
-        <label htmlfor='feedrate-input'>Feedrate:</label>
-        <input type='text' className='form-control' name='feedrate-input' onChange={(e) => setLinearFeedRate(e.target.value)} />
-        {posInput}
-    </div>;
-};
-
     const handleSubmit = () => {
-        if (linearChoice && linearFeedRate && linearPos && linearPos2) {
+        if (linearChoice && linearFeedRate && linearPos) {
             props.setShow(false);
             resetFormValues();
             props.registerSubmit(
                 linearChoice,
                 linearFeedRate,
-                linearPos,
-                linearPos2
+                linearPos
             );
         }
     };

9b699b6a72dc7261eb36cec4244f77862f40f135
Katie <galaxyventurer2000@gmail.com>
2022-04-21 17:06:12 -0600

    rapid  movement done

--- client/src/components/gen-home-modals/rapid-movement-modal.js
+++ client/src/components/gen-home-modals/rapid-movement-modal.js
@@ -3,29 +3,23 @@ import { Modal } from "react-bootstrap";
 
 export default function RapidForm(props) {
     const [rapidChoice, setRapidChoice] = useState('X');
-    const [rapidFeedRate, setRapidFeedRate] = useState(0);
-    const [rapidPos, setRapidPos] = useState(0);
-    const [rapidPos2, setRapidPos2] = useState(0);
+    const [rapidPos, setRapidPos] = useState();
 
     // Validations 
     let rapidChoiceValid = false;
-    let rapidFeedRateValid = false;
     let rapidPosValid = false;
-    let rapidPos2Valid = false;
 
     const resetFormValues = () => {
         setRapidChoice('');
-        setRapidFeedRate('');
         setRapidPos('');
-        setRapidPos2('');
     };
 
-    const rapidFeedRateValidation = () => {
-        if (rapidFeedRate === '') {
-            rapidFeedRateValid = false;
-            return <small className="text-danger">Please Enter The Feed Rate</small>;
+    const rapidChoiceValidation = () => {
+        if (rapidChoice === '') {
+            rapidChoiceValid = false;
+            return <small className="text-danger">Please Enter The Plane of Axis</small>;
         }
-        rapidFeedRateValid = true;
+        rapidChoiceValid = true;
     };
 
     const rapidPosValidation = () => {
@@ -39,14 +33,6 @@ export default function RapidForm(props) {
         rapidPosValid = true;
     };
 
-    const rapidPos2Validation = () => {
-        if (rapidPos2 === '') {
-            rapidPos2Valid = false;
-            return <small className="text-danger">Please Enter A Valid End Position</small>;
-        }
-        rapidPos2Valid = true;
-    };
-
     const RapidForm = () => <form>
         <div className="form-group">
             <label htmlFor="axisOfMovement">Axis of Movement: </label>
@@ -54,20 +40,8 @@ export default function RapidForm(props) {
                 <option value="X">X</option>
                 <option value="Y">Y</option>
                 <option value="Z">Z</option>
-                <option value="XY">XY</option>
             </select>
-            {rapidFormHelper()}
-        </div>
-        <div className="form-group">
-            <label htmlFor="username-input">Feed Rate</label>
-            <input 
-                type="text"
-                className="form-control"
-                name="username-input"
-                value={rapidFeedRate}
-                onChange={(e) => setRapidFeedRate(e.target.value)}
-            />
-            {rapidFeedRateValidation()}
+            {rapidChoiceValidation()}
         </div>
         <div className="form-group">
             <label htmlFor="username-input">Position 1</label>
@@ -80,57 +54,15 @@ export default function RapidForm(props) {
             />
             {rapidPosValidation()}
         </div>
-        <div className="form-group">
-            <label htmlFor="username-input">Email*</label>
-            <input 
-                type="text"
-                className="form-control"
-                name="username-input"
-                value={rapidPos2}
-                onChange={(e) => setRapidPos2(e.target.value)}
-            />
-            {rapidPos2Validation()}
-        </div>
     </form>;
 
-const rapidFormHelper = () => {
-    let posInput;
-    if (rapidChoice === 'X')
-        posInput = <div>
-            <label htmlfor='x-pos-input'>X:</label>
-            <input className='form-control' name='x-pos-input' onChange={(e) => setRapidPos(e.target.value)} />
-        </div>;
-    else if (rapidChoice === 'Y')
-        posInput = <div>
-            <label htmlfor='y-pos-input'>Y:</label>
-            <input className='form-control' name='y-pos-input' onChange={(e) => setRapidPos(e.target.value)} />
-        </div>;
-    else if (rapidChoice === 'Z')
-        posInput = <div>
-            <label htmlfor='z-pos-input'>Z:</label>
-            <input className='form-control' name='z-pos-input' onChange={(e) => setRapidPos(e.target.value)} />
-        </div>;
-    else if (rapidChoice === 'XY')
-        posInput = <div>
-            <label htmlfor='x-pos-input'>X:</label>
-            <input className='form-control' name='x-pos-input' onChange={(e) => setRapidPos(e.target.value)} />
-            <label htmlfor='y-pos-input'>Y:</label>
-            <input className='form-control' name='y-pos-input' onChange={(e) => setRapidPos2(e.target.value)} />
-        </div>;
-    return <div>
-        {posInput}
-    </div>;
-};
-
     const handleSubmit = () => {
-        if (rapidChoiceValid && rapidFeedRateValid && rapidPosValid && rapidPos2Valid) {
+        if (rapidChoiceValid && rapidPosValid) {
             props.setShow(false);
             resetFormValues();
             props.registerSubmit(
                 rapidChoice,
-                rapidFeedRate,
-                rapidPos,
-                rapidPos2
+                rapidPos
             );
         }
     };
